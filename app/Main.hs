456ce2cbcc622014e6bd5fece7f5886ae6ea5270
Chris Barrett <chris.d.barrett@me.com>
2016-08-28 00:02:32 +1200

    Refactor Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,18 @@
+{-# LANGUAGE FlexibleContexts  #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
-{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
 module Main where
 
 import qualified Apidoc.Check                 as Check
 import qualified Apidoc.Json                  as Json
 import qualified Apidoc.Render                as Render
+import           Control.Monad.Reader         as Reader
 import qualified Data.Bifunctor               as Bifunctor
 import qualified Data.ByteString              as Strict
 import           Options.Applicative          as Options
 import qualified System.Environment           as Environment
 import qualified System.Exit                  as Exit
-import           System.IO                    (stderr, stdout)
+import qualified System.IO                    as IO
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
 
@@ -25,7 +25,7 @@ desc prog = fullDesc
     <> header (prog ++ " - apidoc spec validator")
     <> progDesc "Validates an apidoc JSON specification"
 
-opts = Opts
+flags = Opts
     <$> argument str (metavar "FILE" <> help "Path to the apidoc spec to validate")
     <*> switch (long "plain" <> help "Remove ANSI colour codes from output.")
 
@@ -33,22 +33,25 @@ opts = Opts
 main :: IO ()
 main = do
     prog <- Environment.getProgName
-    Opts {optFile = f, .. } <- Options.execParser (info (helper <*> opts) (desc prog))
-
-    let printDoc h d = PP.displayIO h (PP.renderPretty 1 10000 (withColour d))
-        withColour d = if optNoColour then PP.plain d else d
-
-    validateFile f >>= \case
-      Right _ -> do
-          printDoc stdout (PP.dullgreen "Finished with no errors.")
-          Exit.exitSuccess
-      Left errs -> do
-          printDoc stderr errs
-          printDoc stdout (PP.red "Malformed apidoc spec.")
-          Exit.exitFailure
-
+    os <- Options.execParser (info (helper <*> flags) (desc prog))
+    Reader.runReaderT (validateFile >>= either reject accept) os
   where
-    validateFile f = do
-        bs <- Strict.readFile f
-        js <- Json.parseFile f
+    accept _ = do
+        printDoc IO.stdout (PP.dullgreen "Finished with no errors.")
+        liftIO Exit.exitSuccess
+
+    reject errs = do
+        printDoc IO.stderr errs
+        printDoc IO.stdout (PP.red "Malformed apidoc spec.")
+        liftIO Exit.exitFailure
+
+    validateFile = do
+        Opts {optFile = f} <- Reader.ask
+        bs <- liftIO (Strict.readFile f)
+        js <- liftIO (Json.parseFile f)
         pure (js >>= Bifunctor.first (Render.renderErrs bs) . Check.validate)
+
+    printDoc h d = do
+        Opts {optNoColour = noColour} <- Reader.ask
+        let d' = if noColour then PP.plain d else d
+        liftIO (PP.displayIO h (PP.renderPretty 1 80 d'))
