1a5c4d877d0945ce92dcd498bd7d3b8419a8ce12
Chris Barrett <chris.d.barrett@me.com>
2016-08-28 10:42:42 +1200

    WIP error rendering

--- src/Apidoc/Render.hs
+++ src/Apidoc/Render.hs
@@ -2,55 +2,60 @@
 module Apidoc.Render where
 
 import           Apidoc.Check.Err
-import qualified Apidoc.Check.Err             as Check
 import           Apidoc.Check.Lenses
 import           Apidoc.Json.Lenses
+import           Apidoc.Pos
 import           Control.Lens
 import           Data.ByteString              (ByteString)
+import           Data.Foldable                as Foldable
 import           Data.Monoid                  ((<>))
-import           Data.Sequence                (Seq)
 import qualified Data.Text                    as Text
+import           Debug.Trace
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 import qualified Text.Trifecta                as Trifecta
 
-renderErrs :: ByteString -> Seq Check.Err -> PP.Doc
+renderErrs :: (Traversable f, Foldable f) => ByteString -> f Err -> PP.Doc
 renderErrs bs es =
-    case Trifecta.parseByteString raiseErrs mempty bs of
-      Trifecta.Success _ -> mempty
-      Trifecta.Failure doc -> doc
+    trace ("Render errors: " <> show (es ^.. traverse.errPos._Pos)) $ do
+    PP.vcat (renderErr <$> Foldable.toList es)
   where
-    raiseErrs =
-        traverse raiseErrAsMessage es
+    renderErr :: Err -> PP.Doc
+    renderErr e =
+        let r = Trifecta.renderingCaret (e ^. errPos._Pos) bs
+        in Trifecta.explain r (toTrifectaErr e) <> PP.line
 
-    raiseErrAsMessage err =
-      Trifecta.raiseErr . Trifecta.failed . Text.unpack $
-          case err ^. errType of
-            TypeError (Expected e) (Actual a) ->
-                "Type error. Expected: `" <> e <> "`, but got: `" <> a <> "`."
+    toTrifectaErr e = mempty & Trifecta.reason .~ Just (note e)
 
-            RequiredKeyMissing k ->
-                "Missing required key: `" <> k <> "`."
+    text = PP.text . Text.unpack
 
-            UnexpectedKey k ->
-                "Unexpected key: `" <> k <> "`."
+    note err = case err ^. errType of
 
-            InvalidUri _ ->
-                "Invalid URI."
+        TypeError (Expected e) (Actual a) ->
+            "Type error. Expected: ‘" <> text e <> "’, but got: ‘" <> text a <> "’."
 
-            InvalidHttpMethod m ->
-                "Invalid HTTP method: `" <> m <> "`."
+        RequiredKeyMissing k ->
+            "Missing required key: ‘" <> text k <> "’."
 
-            InvalidParameterLocation loc ->
-                "Invalid parameter location: `" <> loc <> "`."
+        UnexpectedKey k ->
+            "Unexpected key: ‘" <> text k <> "’."
 
-            ResponseCodeOutOfRange n ->
-                "Response code out of range: `" <> Text.pack (show n) <> "`."
+        InvalidUri _ ->
+            "Invalid URI."
 
-            UnparseableResponseCode s ->
-                "Invalid response code: `" <> s <> "`."
+        InvalidHttpMethod m ->
+            "Invalid HTTP method: ‘" <> text m <> "’."
 
-            UnparseableTypeRef t ->
-                "Invalid type: `" <> t <> "`."
+        InvalidParameterLocation loc ->
+            "Invalid parameter location: ‘" <> text loc <> "’."
 
-            DuplicateKey _ dup -> do
-                "Duplicated key: `" <> dup ^. keyLabel <> "`."
+        ResponseCodeOutOfRange n ->
+            "Response code out of range: ‘" <> PP.text (show n) <> "’."
+
+        UnparseableResponseCode s ->
+            "Invalid response code: ‘" <> text s <> "’."
+
+        UnparseableTypeRef t ->
+            "Invalid type: ‘" <> text t <> "’."
+
+        DuplicateKey _ dup ->
+            "Duplicated key: ‘" <> text (dup ^. keyLabel) <> "’."
