3d4325964c40301f7ba6e032aca44034a01afe75
Chris Barrett <chris.d.barrett@me.com>
2016-11-22 20:23:20 +1300

    Fix malformed haddocks

--- src/Apidoc/Check/Err.hs
+++ src/Apidoc/Check/Err.hs
@@ -13,21 +13,21 @@ newtype Actual = Actual {_actualLabel :: Text}
 data ErrType = TypeError Expected Actual
              | RequiredKeyMissing Text
              | UnexpectedKey
-                  Text         -- ^ Found key
-                  (Maybe Text) -- ^ Suggestion
+                  Text         -- Found key
+                  (Maybe Text) -- Suggestion
              | InvalidUri Text
              | InvalidHttpMethod
-                   Text         -- ^ Actual method
-                   (Maybe Text) -- ^ Suggestion
+                   Text         -- Actual method
+                   (Maybe Text) -- Suggestion
              | InvalidParameterLocation
-                   Text         -- ^ Actual location
-                   (Maybe Text) -- ^ Suggestion
+                   Text         -- Actual location
+                   (Maybe Text) -- Suggestion
              | ResponseCodeOutOfRange Integer
              | UnparseableResponseCode Text
              | UnparseableTypeRef Text
              | DuplicateKey
-                  Json.Key -- ^ First declaration
-                  Json.Key -- ^ Duplicate declaration
+                  Json.Key -- First declaration
+                  Json.Key -- Duplicate declaration
   deriving (Show, Eq, Ord)
 
 data Err = Err {_errPos :: Pos, _errType :: ErrType}
